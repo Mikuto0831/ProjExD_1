fe05b969e4cf6ebbd69bd387311f7e95de81f0c3
Mikuto <mikuto1227k@gmail.com>
2024-09-24 16:25:26 +0900

    feat:練習8_こうかとんに移動機能を実装

--- pygame_image.py
+++ pygame_image.py
@@ -14,6 +14,9 @@ def main():
     # ここから 練習2
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    # ここから 練習8-1 rectの初期座標設定
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300,200
     # ここまで
 
     tmr = 0 # 時間保存
@@ -22,11 +25,25 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_lst = pg.key.get_pressed() # 練習8-3 全キーの押下状態取得
+        
+        # 練習8-4 方向キーの押下状態を繁栄
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0,-1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0,1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1,0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(1,0)
+        
+
+
         # 練習7
         for i in range(4):
             screen.blit(bg_imgs[i%2], [-(tmr % 3200)+1600*i, 0])
-        
-        screen.blit(kk_img, [300,200])
+         
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
